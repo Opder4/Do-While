676a4db572c44471e796acda2b8eab483130ce28
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:09:44 -0600

    Fin Ejercicio 13

--- Ejercicio 13/Ejercicio 13/Program.cs
+++ Ejercicio 13/Ejercicio 13/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int numero = -1;
+int contador = 0;
+Console.WriteLine("Ingrese los números que quiera e ingrese 0 para mostrar la cantidad de números");
+Console.WriteLine();
+do
+{
+    Console.WriteLine($"Ingrese un número");
+    Console.WriteLine();
+    numero = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    contador++;
+} while (numero != 0);
+Console.WriteLine($"la cantidad total de números ingresados son {contador}");
+
