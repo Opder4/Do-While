93b997bceb40effee87d748d0ccc42ba00f5b33d
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:46:47 -0600

    Fin Ejercicio 18

--- Ejercicio 18/Ejercicio 18/Program.cs
+++ Ejercicio 18/Ejercicio 18/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿int opcion = -1;
+do
+{
+    Console.WriteLine("Menú");
+    Console.WriteLine();
+    Console.WriteLine("1. Mostar Saludo");
+    Console.WriteLine("2. Mostar un número aleatorio");
+    Console.WriteLine("0. Salir");
+    Console.WriteLine("");
+    Console.WriteLine("Seleccione una opción");
+    Console.WriteLine("");
+    opcion=int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Hola");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case 2:
+            Console.WriteLine(Random.Shared.Next(1,1000000));
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case 0:
+            Console.WriteLine("Finalizando programa...");
+            break;
+        default:
+            Console.WriteLine("Opción inválida");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+    }
+} while (opcion != 0);
+Console.WriteLine("Programa finalizado");
