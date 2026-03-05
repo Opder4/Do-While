760a3b72a6144e07e49aa4809c28e32bfa819684
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:51:10 -0600

    Fin Ejercicio 19

--- Ejercicio 19/Ejercicio 19/Program.cs
+++ Ejercicio 19/Ejercicio 19/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Ingrese un número para mostar su tabla de multiplicar: ");
+Console.WriteLine();
+int numero=int.Parse(Console.ReadLine());
+Console.WriteLine();
+int rangotabla = 1;
+do
+{
+    Console.WriteLine($"{numero} X {rangotabla} = {numero*rangotabla}");
+    rangotabla++;
+} while (rangotabla <= 10);
