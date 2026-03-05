9a4c8f3a2dae35aab89b45e4138c76bc32048128
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 19:57:30 -0600

    Fin Ejercicio 11

--- Ejercicio 11/Ejercicio 11/Program.cs
+++ Ejercicio 11/Ejercicio 11/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int cantidadnumero = 1;
+int numero = 0;
+int suma = 0;
+do
+{
+    Console.WriteLine($"Ingrese el {cantidadnumero}° número: ");
+    Console.WriteLine();
+    numero=int.Parse( Console.ReadLine() );
+    Console.WriteLine();
+    suma += numero;
+    cantidadnumero++;
+} while (cantidadnumero <= 5);
+Console.WriteLine($"la suma es {suma}");
