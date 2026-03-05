71411369c1a9683ffc1e8ada8764984f9aee9fdc
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:16:36 -0600

    Fin Ejercicio 14

--- Ejercicio 14/Ejercicio 14/Program.cs
+++ Ejercicio 14/Ejercicio 14/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int numero = -1;
+int numeromayor = int.MinValue;
+Console.WriteLine("Ingrese los números que quiera e ingrese 0 para mostrar la suma final");
+Console.WriteLine();
+do
+{
+    Console.WriteLine($"Ingrese un número");
+    Console.WriteLine();
+    numero = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numeromayor<numero)
+    {
+        numeromayor = numero;
+    }
+} while (numero != 0);
+Console.WriteLine($"El número mayor fue {numeromayor}");
+
