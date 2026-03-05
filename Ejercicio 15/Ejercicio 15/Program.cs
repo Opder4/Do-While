2eb20f7997239fb17c879423def557cb31f25325
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:23:24 -0600

    Fin Ejercicio 15

--- Ejercicio 15/Ejercicio 15/Program.cs
+++ Ejercicio 15/Ejercicio 15/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int numero = -1;
+int numero2 = 0;
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese un número positivo: ");
+    Console.WriteLine();
+    numero=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    numero2 = numero;
+    if (numero2 == 0)
+    {
+        Console.WriteLine("El número 0 no es positivo ni negativo");
+    }
+} while (numero <= 0);
+    Console.WriteLine("Gracias por ingresar un número positivo");
