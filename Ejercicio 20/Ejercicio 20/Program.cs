4925a981f9ad373a5946bf3fb71a7bac6f968a5a
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 21:01:59 -0600

    Fin Ejercicio 20

--- Ejercicio 20/Ejercicio 20/Program.cs
+++ Ejercicio 20/Ejercicio 20/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.WriteLine("Ingrese los número que quiera");
+Console.WriteLine();
+Console.WriteLine("Presione 0 para mostar:");
+Console.WriteLine();
+Console.WriteLine("- La suma total de los números");
+Console.WriteLine("- Cuántos números se ingresaron");
+Console.WriteLine("- El promedio de los número ingresados");
+Console.WriteLine();
+int numero = -1;
+int suma = 0;
+int contador = 0;
+do
+{
+    Console.WriteLine("Ingrese un número: ");
+    Console.WriteLine();
+    numero=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    suma += numero;
+    contador++;
+} while (numero != 0);
+Console.WriteLine($"La suma total de los números es {suma}");
+Console.WriteLine();
+Console.WriteLine($"La cantidad total de números ingresados son {contador-1}");
+Console.WriteLine("");
+Console.WriteLine($"El promedio de los números ingresador es {suma/(contador-1)}");
