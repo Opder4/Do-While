4d202f476ba40fb5705897eda010d460bd3885ad
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:26:23 -0600

    Fin Ejercicio 16

--- Ejercicio 16/Ejercicio 16/Program.cs
+++ Ejercicio 16/Ejercicio 16/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Threading.Channels;
+
+string contra = "";
+do
+{
+    Console.WriteLine("Ingrese la contraseña: ");
+    Console.WriteLine();
+    contra=Console.ReadLine();
+    Console.WriteLine();
+} while (contra != "1234");
+Console.WriteLine("Contraseña correcta");
+Console.WriteLine();
+Console.WriteLine("Puede ingresar");
