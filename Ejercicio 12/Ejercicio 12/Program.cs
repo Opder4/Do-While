6de79c67c7b3682a08dd4db417b425406c70f120
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:04:52 -0600

    Fin Ejercicio 12

--- Ejercicio 12/Ejercicio 12/Program.cs
+++ Ejercicio 12/Ejercicio 12/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int numero = -1;
+int suma = 0;
+int ordinales = 1;
+Console.WriteLine("Ingrese los números que quiera sumar e ingrese 0 para mostrar la suma final");
+Console.WriteLine();
+do
+{
+    Console.WriteLine($"Ingrese el {ordinales}° número");
+    Console.WriteLine();
+    numero=int.Parse( Console.ReadLine() );
+    Console.WriteLine();
+    suma += numero;
+    ordinales++;
+} while (numero != 0);
+Console.WriteLine($"la suma total es {suma}");
