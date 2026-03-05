b2c7788038ef60ee150dbff49d2c08737eba2f59
Emanuel Ramírez <er6348100@gmail.com>
2026-03-04 20:36:27 -0600

    Fin Ejercicio 17

--- Ejercicio 17/Ejercicio 17/Program.cs
+++ Ejercicio 17/Ejercicio 17/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int numeros = -1;
+int contador = 0;
+Console.WriteLine("Ingrese los números que quiera y presione 0 para ver cuantos son pares");
+Console.WriteLine();
+do
+{
+    Console.WriteLine("Ingrese un número: ");
+    Console.WriteLine();
+    numeros=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numeros%2==0)
+    {
+        contador++;
+    }
+
+} while (numeros != 0);
+Console.WriteLine($"La cantidad de números pares ingresados son {contador-1}");
